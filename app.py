f32ec4fefa50c2daf44c0d1470eede28938aac9a
Eduardo <eduardo.navarro2@unmsm.edu.pe>
2022-05-12 21:07:08 -0500

    Add window code

--- app.py
+++ app.py
@@ -1,100 +1,62 @@
+import tkinter as tk
 import os
-import sys
-from tkinter.filedialog import Open
+from PIL import ImageTk,Image
+from tkinter import filedialog       #Obtenga la ruta completa del archivo
+from tkinter import ttk
 
-from cloudinary.api import delete_resources_by_tag, resources_by_tag
-from cloudinary.uploader import upload
-from cloudinary.utils import cloudinary_url
-import cloudinary
-""" 
-# config
-os.chdir(os.path.join(os.path.dirname(sys.argv[0]), '.'))
-if os.path.exists('settings.py'):
-    exec(open('settings.py').read())
+ventana=tk.Tk()   #Crear objeto
 
-cloudinary.config( 
-  cloud_name = "doh7eom1j", 
-  api_key = "218576176667494", 
-  api_secret = "XIIe3Gl9T6f11Ei4jUcgLX8hrlI",
-)
+ancho_ventana = 900
+alto_ventana = 600
 
-DEFAULT_TAG = "python_sample_basic"
+x_ventana = ventana.winfo_screenwidth() // 2 - ancho_ventana // 2
+y_ventana = ventana.winfo_screenheight() // 2 - alto_ventana // 2
 
+posicion = str(ancho_ventana) + "x" + str(alto_ventana) + "+" + str(x_ventana) + "+" + str(y_ventana)
+ventana.geometry(posicion)
 
-def dump_response(response):
-    print("Upload response:")
-    for key in sorted(response.keys()):
-        print("  %s: %s" % (key, response[key]))
+ventana.title('UNMSM-FISI | Software-Inteligente')
+ventana.iconbitmap('img/artificialintelligence.ico')
+ventana.resizable(0,0)
 
+# l=tk.Label(ventana,text='Imagen en baja calidad para procesar', image=None)   #Crea una etiqueta
+# l.place(x=50,y=10)
 
-def upload_files():
+comboStyle = ttk.Combobox(state="readonly", values=["Art", "Photo"])
+comboStyle.place(x=50, y=50)
 
-    print("--- Upload a local file with custom public ID")
+comboNoise = ttk.Combobox(state="readonly", values=["-1", "0", "1", "2", "3"])
+comboNoise.place(x=50, y=150)
 
-    response = upload( "pizza.jpg",
-        tags=DEFAULT_TAG,
-    )
-    dump_response(response)
-    url, options = cloudinary_url(
-        response['public_id'],
-        format=response['format'],
-        width=200,
-        height=150,
-        crop="fit"
-    )
-    print("Fit into 200x150 url: " + url)
-    print("")
- """
+comboX2 = ttk.Combobox(state="readonly", values=["1", "2", "3", "4"])
+comboX2.place(x=50, y=250)
 
-################# GUI    
-""" import tkinter as tk
-import tkinter.filedialog
-import requests
-import json
+frame = tk.Frame()
+frame.pack()      
 
-ventana = tk.Tk()
-imagen = None
+# Como no tenemos ningún elemento dentro del frame, 
+# no tiene tamaño y aparece ocupando lo mínimo posible, 0*0 px
 
-def abrirArchivo():
-    global imagen
-    archivo_abierto = tk.filedialog.askopenfilename(initialdir="/", 
-                      title="Seleccione archivo", filetypes=(("jpg files", "*.jpg"),("all files", "*.*")))
-    print(archivo_abierto)
-    if (archivo_abierto):
-        imagen = tk.PhotoImage(file=archivo_abierto)
-        lbImagen = tk.Label(ventana, image=imagen).place(relx=.5, rely=.1,relwidth=.4,relheight=.5)
+# Color de fondo, background
+frame.config(bg="lightblue")     
+
+# Podemos establecer un tamaño,
+# la raíz se adapta al frame que contiene
+frame.config(width=480,height=320) 
+l=tk.Label(frame,text='Imagen en baja calidad para procesar', image=None)   #Crea una etiqueta
+l.place(x=50,y=10)
+
+def openpicture():
+    global img
+    filename=filedialog.askopenfilename(initialdir=os.getcwd(),filetypes=[("jpg files", "*.jpg")])     #Obtenga la ruta completa del archivo
+    img=ImageTk.PhotoImage(Image.open(filename))   #tkinter solo puede abrir archivos gif, aquí use la biblioteca PIL
+    print(filename)
+    l.config(image=img)    #Utilice el método de configuración para colocar la imagen en la etiqueta
+ 
 
 
-ventana.geometry("500x500")
-tk.Button(text="Abrir archivo", bg="pale green", command=abrirArchivo).place(relx=.10, rely=.10, relheight=.1, relwidth=.3)
+b=tk.Button(ventana,text='Seleccione una imagen', command=openpicture)  # Configure el botón y dele el comando openpicture
+b.place(x=50, y=100)
 
-ventana.mainloop() """
+tk.mainloop()
 
-
-
-
-
-######################## bigjpg
-""" import requests
-import json
-
-data = {
-        'style': 'art',
-        'noise': '3',
-        'x2': '1',
-        'input': 'url'
-        
-}
-
-r = requests.post(
-        url='https://bigjpg.com/api/task/',
-        headers={'X-API-KEY': '21eb74b866894ad9b751cbfe09e2ddd3'},
-        data={'conf': json.dumps(data)}
-    )
-print(r.json())
-
-
-r = requests.get(url='https://bigjpg.com/api/task/#########')
-print(r.json()) 
-#devulve un url para descargar la imagen
-"""
